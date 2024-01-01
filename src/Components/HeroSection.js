6ca4e5ef4389c495ab36fc39d9fd9bea0c2f7ab3
SamsonI95 <samsoniweibo@gmail.com>
2024-01-01 01:01:02 +0100

    updates

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import "./HeroSection.css";
 import { Button } from "./Button";
 import { PrcData } from "./PrcData";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 function HeroSection() {
   const [button, setButton] = useState(true);
@@ -44,28 +46,54 @@ function HeroSection() {
       <div className="donate-start-card1">
         <img src="assets/africa-humanitarian-aid-doctor-taking-care-patient.svg" />
         <h3>Join us, Donate Blood and Save Lives</h3>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy </p>
-        {button && (<Button buttonStyle="btn--primary" buttonSize="btn--small">Get Started</Button>)}
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy{" "}
+        </p>
+        {button && (
+          <Button buttonStyle="btn--primary" buttonSize="btn--small">
+            Get Started
+          </Button>
+        )}
       </div>
       <div className="product-card1-container">
-        {
-          PrcData.map((d) => (
-            <>
-              <div className="product-card1">
-                <div className="card-image">
-                  <img src={image} />
-                </div>
-                <div className="card-text">
-                  <h3>{title}</h3>
-                  <p>{description}</p>
-                </div>
-                <div className="card-button">
-                {button && (<Button buttonStyle="btn--primary" buttonSize="btn--small">{buttonName}</Button>)}
-                </div>
-              </div>
-            </>
-          ))
-        }
+        {PrcData.map((d) => (
+          <div className="product-card1">
+            <div className="card-image">
+              <img src={d.image} alt="/" />
+            </div>
+            <div className="card-text">
+              <h3>{d.title}</h3>
+              <p>{d.description}</p>
+            </div>
+            <div className="card-button">
+              {button && (
+                <Button buttonStyle="btn--primary" buttonSize="btn--medium">
+                  {d.buttonName} <FontAwesomeIcon icon={faArrowRight} />
+                </Button>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="discovery-container">
+        <div className="dis-header">
+          <h3>Discover how easy it is to become a hero</h3>
+        </div>
+        <div className="dis-content-card">
+          <img src="assets/afr-doc.jpg" alt=""/>
+          <ul>
+            <li>Register by Creating your donor profile</li>
+            <li>Find a center by Locating a donation center near you</li>
+            <li>Donate: by Giving the gift of life</li>
+            <li>Track impact to See your contribution in real-time</li>
+          </ul>
+        </div>
+        {button && (
+          <Button buttonStyle="btn--primary" buttonSize="btn--small">
+            Get Started
+          </Button>
+        )}
       </div>
     </>
   );
